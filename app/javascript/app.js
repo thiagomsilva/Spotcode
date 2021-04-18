0a5e28f408dfaca410a13793c11dbc3331b3d4b6
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2021-04-18 01:20:58 -0300

    add routes, styles components, icons and top menu

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,12 +1,17 @@
 import React, { Fragment } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Button } from 'react-bulma-components';
-import 'app.scss';
+import Routes from "./routes"
+import { BrowserRouter } from "react-router-dom"
+import Menu from "./components/common/menu"
+// import 'app.scss';
 
 const App = () => {
   return(
       <Fragment>
-          <Button color='primary'>Teste</Button>
+          <BrowserRouter>
+            <Menu/>
+            <Routes/>
+          </BrowserRouter>
       </Fragment>
   );
 }
